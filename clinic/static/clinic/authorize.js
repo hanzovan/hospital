788c1ce7c2ad300a74ca2593916c0f47d8e5963e
Archer <thivan.nms@gmail.com>
2023-08-22 16:29:55 +0700

    Raise a confirmation when top admin set a new level to a user

--- clinic/static/clinic/authorize.js
+++ clinic/static/clinic/authorize.js
@@ -1,27 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.user-info').forEach(info => {
 
-        info.querySelector('.level').onchange = function() {
-            const newLevel = this.value;
-            const chosenUserID = info.id.slice(4);
+        // Store the original level
+        const originalLevel = info.querySelector('.level').value;
 
-            fetch('authorize', {
-                method: 'POST',
-                body: JSON.stringify({
-                    'new_level': newLevel,
-                    'chosen_user_id': chosenUserID
+        info.querySelector('.level').onchange = function() {
+            const confirmMessage = 'Are you really want to set new level for this user?';
+            const isConfirmed = confirm(confirmMessage);            
+
+            if (isConfirmed) {
+                const newLevel = this.value;
+                const chosenUserID = info.id.slice(4);
+
+                fetch('authorize', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        'new_level': newLevel,
+                        'chosen_user_id': chosenUserID
+                    })
                 })
-            })
-            .then(response => response.json())
-            .then(() => {
-                info.querySelector('.current-level').innerHTML = newLevel;
-            })
-            .catch(error => {
-                console.log(error);
-            })
+                .then(response => response.json())
+                .then(() => {
+                    info.querySelector('.current-level').innerHTML = newLevel;
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            } else {
+                this.value = originalLevel;
+            }            
         }
     
     })
 })
-
-// Add confirm before change select
