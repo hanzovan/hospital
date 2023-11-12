81f21669c6fd76b4dc2f460e6c310ca948bbe7c4
Archer <thivan.nms@gmail.com>
2023-11-12 12:04:13 +0700

    Modify add_contract template to a new version with better design

--- clinic/static/clinic/add_contract.js
+++ clinic/static/clinic/add_contract.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // When page load, start to move the form from top to center
     const formContainer = document.querySelector('.form-container');
     formContainer.style.opacity = '0';
     setTimeout(function() {
         formContainer.style.opacity = '1';
         formContainer.style.transform = 'translateY(0)';
     }, 50);
+
+    // When user click on an input field, show the label
+    const Controls = document.querySelectorAll('.form-control');
+    Controls.forEach(control => {
+        control.onfocus = function() {
+            this.closest('.form-group').classList.add('focused');
+        }
+        control.onblur = function() {
+            this.closest('.form-group').classList.remove('focused');
+        }
+    })
 })
